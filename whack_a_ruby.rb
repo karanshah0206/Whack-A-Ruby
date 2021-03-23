2d34342d5df1d812d5ecb78dda291ecaa34ffa06
Karan Shah <karanshah0206@outlook.com>
2021-03-23 08:03:58 +0530

    Ruby Movement Definition with Edge Countermeasures

--- whack_a_ruby.rb
+++ whack_a_ruby.rb
@@ -8,6 +8,20 @@ class GameWindow < Gosu::Window
         @ruby = Gosu::Image.new("./media/ruby.png")
         @ruby_x = 25
         @ruby_y = 25
+        @ruby_vx = 5
+        @ruby_vy = 5
+    end
+
+    def update
+        @ruby_x += @ruby_vx
+        @ruby_y += @ruby_vy
+        # Ruby Reaches Edges
+        if (@ruby_x -25 <= 0) or (@ruby_x + 25 >= 800)
+            @ruby_vx *= -1
+        end
+        if (@ruby_y -25 <= 0) or (@ruby_y + 25 >= 600)
+            @ruby_vy *= -1
+        end
     end
 
     def draw
