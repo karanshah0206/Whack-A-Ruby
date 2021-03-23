6e65cb71f07a7dfdd4c818d843109be87207e298
Karan Shah <karanshah0206@outlook.com>
2021-03-23 07:58:12 +0530

    Ruby Image Configuration

--- whack_a_ruby.rb
+++ whack_a_ruby.rb
@@ -4,6 +4,14 @@ class GameWindow < Gosu::Window
     def initialize
         super 800, 600, false
         self.caption = "Whack A Ruby"
+        # Ruby Image Configuration
+        @ruby = Gosu::Image.new("./media/ruby.png")
+        @ruby_x = 25
+        @ruby_y = 25
+    end
+
+    def draw
+        @ruby.draw(@ruby_x - 25, @ruby_y - 25, z=1, 0.0625, 0.0625)
     end
 end
 
